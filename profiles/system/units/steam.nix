f4db37d15aa2aed2274b41eba5cd9ee16e7b3b3d
prtzl <matej.blagsic@protonmail.com>
2026-02-14 21:12:25 +0100

    Update drivers for steam and gpu

--- profiles/system/units/steam.nix
+++ profiles/system/units/steam.nix
@@ -1,8 +1,12 @@
 {
   lib,
+  pkgs,
   ...
 }:
 
+let
+  pkgs-steam = pkgs.pkgs-unstable;
+in
 {
   # Allow steam specifically
   nixpkgs.config.allowUnfreePredicate =
@@ -16,8 +20,32 @@
 
   programs.steam = {
     enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    extraCompatPackages = with pkgs-steam; [ proton-ge-bin ];
+    gamescopeSession.enable = true;
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    package = pkgs-steam.steam;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
   };
+
+  programs.gamemode = {
+    enable = true;
+  };
+
+  programs.gamescope = {
+    enable = true;
+    package = pkgs-steam.gamescope;
+    capSysNice = true;
+  };
+
+  security.rtkit = {
+    enable = true;
+    package = pkgs-steam.rtkit;
+  };
+
+  # environment.sessionVariables = {
+  #   MESA_LOADER_DRIVER_OVERRIDE = "iris";
+  #   ANV_ENABLE_PIPELINE_CACHE = "1";
+  #   VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/intel_icd.x86_64.json";
+  # };
 }
