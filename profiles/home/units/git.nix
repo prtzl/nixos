bf902bff734e9e64457b0f9c17a4dc9ca8338187
prtzl <matej.blagsic@protonmail.com>
2026-04-01 12:02:43 +0200

    Update git settings.

--- profiles/home/units/git.nix
+++ profiles/home/units/git.nix
@@ -11,8 +11,14 @@
     enable = true;
     lfs.enable = true;
     settings = {
+      core.autocrlf = false;
       core.init.defaultBranch = lib.mkDefault "master";
+      fetch.prune = true;
       log.date = lib.mkDefault "iso";
+      pull.rebase = true;
+      push.autoSetupRemote = true;
+      rebase.autoStash = true;
+
       # idea is that dir is owned by root but
       # maintainers are in group that has access
       # Git doesn't like this, so I'm placing this here
