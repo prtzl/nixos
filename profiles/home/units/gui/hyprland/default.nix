f235abdafe6521026cc1d55d00fe186d37d072af
prtzl <matej.blagsic@protonmail.com>
2026-06-04 00:12:17 +0200

    Seems like wlsunsset or hyprland or whatever systemd targets and whatnots were fixed. Now wlsunset starts nicely

--- profiles/home/units/gui/hyprland/default.nix
+++ profiles/home/units/gui/hyprland/default.nix
@@ -61,12 +61,6 @@
 
   # Screen color without affecting screenshot/screenrecording SW
   # One of the two following entries (units or restart) makes this work now. There is a race condition that makes the wlsunset start before the wayland is really up
-  systemd.user.services.wlsunset.Unit = {
-    After = lib.mkForce [ "hyprland-session.target" ];
-    Wants = [ "hyprland-session.target" ];
-
-    ConditionEnvironment = lib.mkForce "";
-  };
   services.wlsunset = {
     package = pkgs.wlsunset;
     enable = true;
