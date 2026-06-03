e6494d6b3c5ff25190c0a3f9825aa03a71c6038e
prtzl <matej.blagsic@protonmail.com>
2026-06-03 22:13:42 +0200

    Add hyprland cachix??

--- profiles/system/units/hyprland.nix
+++ profiles/system/units/hyprland.nix
@@ -16,6 +16,12 @@ in
     xwayland.enable = true;
   };
 
+  nix.settings = {
+    substituters = [ "https://hyprland.cachix.org" ];
+    trusted-substituters = [ "https://hyprland.cachix.org" ];
+    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+  };
+
   # Light weight and nice
   services.displayManager.ly = {
     enable = true;
