8648da93ad6b73002aea15116073019f7d1d6eb8
prtzl <matej.blagsic@protonmail.com>
2026-06-28 15:05:03 +0200

    Remove old hyprland conf. Add hyprlock and hypridle

--- profiles/home/units/gui/hyprland/default.nix
+++ profiles/home/units/gui/hyprland/default.nix
@@ -14,6 +14,8 @@
     wl-clipboard # clipboard (why is this additional, like  what?)
     networkmanagerapplet # brings network manager applet functionality
     grimblast # screenshot utility
+    wlopm # wayland power management
+    brightnessctl # controls screen and keyboard backglight brightness
   ];
 
   # The jummy thing about this is that now as a service it reloads on configurations change automatically!
@@ -69,5 +71,128 @@
     temperature.night = 4000;
     latitude = 0;
     longitude = 0;
+  };
+
+  programs.hyprlock = {
+    enable = true;
+    settings = {
+      general = {
+        grace = 1;
+        hide_cursor = true;
+        ignore_empty_input = true;
+      };
+      background = {
+        monitor = "";
+        path = "${./doom.jpg}";
+        blur_size = 5;
+        blur_passes = 1;
+        noise = 0.0117;
+        contrast = 1.3000;
+        brightness = 0.8000;
+        vibrancy = 0.2100;
+        vibrancy_darkness = 0.0;
+      };
+
+      input-field = [
+        {
+          monitor = "";
+          size = "100, 100";
+          outline_thickness = 40;
+          outer_color = "$color5";
+          inner_color = "$color0";
+          font_color = "$color12";
+          fade_on_empty = true;
+          placeholder_text = "<i>Password...</i>"; # Text rendered in the input box when it's empty.;
+          hide_input = true;
+          position = "0, 220";
+          halign = "center";
+          valign = "bottom";
+        }
+      ];
+
+      # Date;
+      label = [
+        {
+          monitor = "";
+          text = "cmd[update:18000000] echo \"<b> $(date +'%-d %B %Y') </b>\"";
+          color = "$color12";
+          font_size = 34;
+          font_family = "Noto Sans 10";
+
+          position = "0, -200";
+          halign = "center";
+          valign = "top";
+        }
+
+        # Day;
+        {
+          monitor = "";
+          text = "cmd[update:18000000] echo \"<b> $(date +'%A') </b>\"";
+          color = "$color5";
+          font_size = 34;
+          font_family = "Noto Sans 10";
+          position = "0, -150";
+          halign = "center";
+          valign = "top";
+        }
+
+        # Time;
+        {
+          monitor = "";
+          text = "cmd[update:1000] echo \"<b><big> $(date +'%H:%M:%S') </big></b>\"";
+          color = "$color15";
+          font_size = 94;
+          font_family = "Noto Sans 10";
+
+          position = "0, 0";
+          halign = "center";
+          valign = "center";
+        }
+
+        # User;
+        {
+          monitor = "";
+          text = "   $USER";
+          color = "$color12";
+          font_size = 18;
+          font_family = "Inter Display Medium";
+
+          position = "0, 100";
+          halign = "center";
+          valign = "bottom";
+        }
+      ];
+    };
+  };
+
+  # A bunch of services/routines based on timeouts related to GUI/hyprland
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        after_sleep_cmd = "wlopm --on '*'";
+        before_sleep_cmd = "pidof hyprlock || hyprlock";
+      };
+      listener = [
+        # Dim monitor after 30s
+        {
+          timeout = 30;
+          on-resume = "brightnessctl -r";
+          on-timeout = "brightnessctl -s set 10";
+        }
+        # Disable keyboard backlight after 5s
+        {
+          timeout = 5;
+          on-resume = "brightnessctl -rd tpacpi::kbd_backlight";
+          on-timeout = "brightnessctl -sd tpacpi::kbd_backlight set 0";
+        }
+        # disables screen after 1min
+        {
+          timeout = 60;
+          on-resume = "wlopm --on '*'";
+          on-timeout = "wlopm --off '*'";
+        }
+      ];
+    };
   };
 }
